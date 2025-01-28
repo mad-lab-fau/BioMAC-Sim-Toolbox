d2e2b0ef68492c0e2a71d535161ae768732c8e81
Anne Koelewijn <anne.koelewijn@fau.de>
2025-01-28 15:10:40 +0100

    Update build_docs.sh
    Fixing doxygen?

--- docs/Doxygen/build_docs.sh
+++ docs/Doxygen/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp ./Doxyfile ./Doxyfile.back
-sed -i 's/m=m2cpp.pl/m="perl m2cpp.pl"/' ./Doxyfile
+sed -i 's/m=m2cpp.pl/m="perl .\/m2cpp.pl"/' ./Doxyfile
 doxygen ./Doxyfile
 mv ./Doxyfile.back ./Doxyfile
