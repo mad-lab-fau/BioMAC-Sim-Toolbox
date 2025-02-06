1d3905fdf54bed31338a67fc065e5197023e9406
Anne Koelewijn <anne.koelewijn@fau.de>
2025-02-06 11:06:01 +0100

    Update build_docs.sh
    Change path

--- docs/Doxygen/build_docs.sh
+++ docs/Doxygen/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp ./Doxyfile ./Doxyfile.back
+cp ./docs/Doxygen/Doxyfile ./docs/Doxygen/Doxyfile.back
 sed -i 's/m=m2cpp.pl/m="perl .\/m2cpp.pl"/' ./Doxyfile
-doxygen ./Doxyfile
-mv ./Doxyfile.back ./Doxyfile
+doxygen ./docs/Doxygen/Doxyfile
+mv ./docs/Doxygen/Doxyfile.back ./docs/Doxygen/Doxyfile
